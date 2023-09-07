421b3acb146d40d76cbb441be9ca080095d8d7d3
ziwanarp <ziwanarizwan@gmail.com>
2023-09-07 22:19:45 +0700

    init

---
+++ database/migrations/2023_09_07_134158_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('no_pesanan',20);
+            $table->dateTime('tanggal');
+            $table->string('nm_pelanggan', 50);
+            $table->string('nm_produk', 50);
+            $table->float('total');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Aplikasi Tiket Konser</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    {{-- tiitle --}}
+    <div class="mt-5 text-center">
+        <h2><u>PRODUCT LIST</u></h2>
+    </div>
+    
+
+<div class="container">
+    <div class="row col-lg-12">
+        <div class="my-3">
+            <div class="text-right">
+                <button class="btn btn-primary ml-auto">Show Products</button>
+            </div>
+        <table class="table table-striped table-bordered">
+            <thead>
+              <tr class="text-center">
+                <th scope="col">Image</th>
+                <th scope="col">Tittle</th>
+                <th scope="col">Category</th>
+                <th scope="col">Brand</th>
+                <th scope="col">Stock</th>
+                <th scope="col">price</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th class="text-center"><img src="https://i.dummyjson.com/data/products/1/thumbnail.jpg" alt="" width="50px"></th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>1</td>
+                <td>200</td>
+                <td>act</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    </div>
+</div>
+      
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+</html>
